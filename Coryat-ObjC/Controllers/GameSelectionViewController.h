377182a3663fe6518293db06e551c79c0be8f5aa
Michael Frain <michael.frain@hudl.com>
2015-02-08 15:11:55 -0800

    Storyboard changes

---
+++ Coryat-ObjC/Controllers/GameSelectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameSelectionViewController.h
+//  Coryat-ObjC
+//
+//  Created by Michael Frain on 2/8/15.
+//  Copyright (c) 2015 Michael Frain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameSelectionViewController : UIViewController
+
+@end
